7add7c8a1994f9a4d527c13148f827ae09195e54
William Strausser <william.e.strausser@gmail.com>
2022-11-27 09:56:08 +0000

    Created index page

--- d3_dataviz/app.py
+++ d3_dataviz/app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/unit1")
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route("/unit1")
+def unit1():
     return render_template("unit1.html")
 
 if __name__== "__main__":
