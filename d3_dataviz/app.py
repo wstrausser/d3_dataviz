bb65964f12149ac7136cbcd7490276ae1254c041
William Strausser <william.e.strausser@gmail.com>
2022-11-28 01:15:04 +0000

    Added unit2 page

--- d3_dataviz/app.py
+++ d3_dataviz/app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/unit1")
+@app.route("/units/unit1")
 def unit1():
     return render_template("unit1.html")
+
+@app.route("/units/unit2")
+def unit2():
+    return render_template("unit2.html")
 
 if __name__== "__main__":
     app.run(debug=True)
