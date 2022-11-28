4a1deefc482bb9a76a7f0c333dea136ca519d7f3
William Strausser <william.e.strausser@gmail.com>
2022-11-28 06:22:14 +0000

    Added vertical lines

--- d3_dataviz/static/unit2/unit2.1.js
+++ d3_dataviz/static/unit2/unit2.1.js
@@ -1,31 +1,88 @@
 function main() {
     const width = document.querySelector('#content').offsetWidth;
     const height = width;
+    const xpad = width/6;
+    const ypad = height/6;
 
     const svg = d3.select('#content')
         .append('svg')
         .attr('width', width)
         .attr('height', height);
-    
-    const marks = [];
 
-    for (let i = 0; i < 100; i++) {
-        marks.push(
+    const bg = svg.append('rect');
+
+    bg.attr('class', 'bg')
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('width', width)
+        .attr('height', height);
+    
+    const vbars = 25;
+
+    const xArr = [];
+    const barThickness = (width-(xpad*2))/((vbars*2)-1);
+    for (let i = 0; i < vbars; i++) {
+        let barClass = undefined
+        if (i%2 == 0) {
+            barClass = "bar1"
+        }
+        else {
+            barClass = "bar2"
+        }
+        xArr.push(
             {
-                y: i * 20,
-                width: width,
-                height: 10
+                x: (i*(barThickness*2)+xpad),
+                y: ypad,
+                width: barThickness,
+                height: height-(ypad*2),
+                class: barClass
             }
         )
-    }
-    console.log(marks);
+    };
 
-    svg.selectAll('rect')
-        .data(marks)
-        .join('rect')
+    svg.selectAll('.vbar').data(xArr).join('rect')
+        .attr('class', (d) => d.class)
+        .attr('x', (d) => d.x)
+        .attr('y', (d) => d.y)
+        .attr('width', (d) => d.width)
+        .attr('height', (d) => d.height);
+    
+    const hbars = vbars;
+    yArr = []
+    for (let i = 0; i < hbars; i++) {
+        const y = (i*(barThickness*2))+ypad;
+        const ylim = height-ypad;
+        if (y > ylim) {
+            break
+        };
+
+        let barClass = undefined;
+        if (i%2 == 0) {
+            barClass = 'bar1';
+        }
+        else {
+            barClass = 'bar2';
+        };
+
+        const barLength = width-(xpad*2);
+
+        yArr.push(
+            {
+                x: xpad,
+                y: y,
+                width: barLength,
+                height: barThickness,
+                class: barClass
+            }
+        );
+    };
+
+    svg.selectAll('.hbar').data(yArr).join('rect')
+        .attr('class', (d) => d.class)
+        .attr('x', (d) => d.x)
         .attr('y', (d) => d.y)
         .attr('width', (d) => d.width)
         .attr('height', (d) => d.height);
 };
 
-main()
+main();
